91257ca687a6bdb7d992c77faa9c79d5f9dac3f4
Rasulwebs <boboqulovrasul80@gmail.com>
2023-04-04 23:57:00 +0500

    editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,29 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
-
-import 'aos/dist/aos.css';
-import './css/style.css';
-
-import AOS from 'aos';
-
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ResetPassword from './pages/ResetPassword';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import "aos/dist/aos.css";
+import "./css/style.css";
+import AOS from "aos";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
     AOS.init({
       once: true,
-      disable: 'phone',
+      disable: "phone",
       duration: 600,
-      easing: 'ease-out-sine',
+      easing: "ease-out-sine",
     });
   });
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
   }, [location.pathname]); // triggered on route change
 
   return (
